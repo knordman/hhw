937046cd7fe0a038ad8fa92cfc421ce11dcbdf13
Kristian Nordman <knordman@github.cxz.fi>
2026-05-31 15:48:23 +0300

    tweak: hash routing

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import GalleryView from './views/GalleryView.vue'
 import FigureView from './views/FigureView.vue'
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     { path: '/', name: 'gallery', component: GalleryView },
     { path: '/figure/:id', name: 'figure', component: FigureView, props: true },
